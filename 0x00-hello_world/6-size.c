d34c923b8d9d17dbd0e82e18786450ff5e7ea34f
alt3ralus <755@holbertonschool.com>
2019-02-07 03:56:28 +0000

    fix variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+int a;
+char b;
+float c;
+long long int d;
+long int e;
+
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu bytes(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long: %lu byte(s)\n", (unsigned long) sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 }
