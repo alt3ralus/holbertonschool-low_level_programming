704aef5409e1d7757fbdc155196a8598a26fa9ec
alt3ralus <755@holbertonschool.com>
2019-02-09 23:51:52 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry block
+ * @void: sto cazzo
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int x = 97;
+
+	while ( x < 123)
+	{
+		putchar(x);
+		x++;
+	}
+
+	putchar('\n');
+	return (0);
+}
