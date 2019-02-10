704bb35f67364c171c431bb0fdc43ea350374476
alt3ralus <755@holbertonschool.com>
2019-02-10 00:51:51 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry block
+ * @void: sto cazzo
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int x = 48;
+
+	while (x < 58)
+	{
+		putchar(x);
+		x++;
+	}
+
+	putchar('\n');
+	return (0);
+}
