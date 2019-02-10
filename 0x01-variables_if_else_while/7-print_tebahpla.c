53459de9cbf453a2be8b38b39fec46c2425bd933
alt3ralus <755@holbertonschool.com>
2019-02-10 03:41:59 +0000

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry block
+ * @void: sto cazzo
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int x = 122;
+
+	while (x > 96)
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
