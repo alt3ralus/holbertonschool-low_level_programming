682aca060315b2137472217e1f212c13bbc60ef3
alt3ralus <755@holbertonschool.com>
2019-02-12 20:13:30 +0000

    Sto cazzo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+g#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int x = 97;
+	int y = 122;
+
+    return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,4 +2,5 @@
 #define holberton
 
 int _putchar(int c);
+int print_alphabet(int c);
 #endif
