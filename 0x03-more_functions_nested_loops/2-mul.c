7421e23f834a62cc1ff93d662540fc4a44e19649
alt3ralus <755@holbertonschool.com>
2019-02-14 22:08:13 +0000

    task 2 header fixed

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - A function that multiplies 2 int's.
+ * @a: Fisrt user parameter.
+ * @b: Second user parameter.
+ * Return: a * b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
