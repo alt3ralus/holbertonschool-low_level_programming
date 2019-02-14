d465c18af91d976b1f4ddd19064a18513eb8b52c
alt3ralus <755@holbertonschool.com>
2019-02-14 22:34:22 +0000

    task 3 pre

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+**/
+* print_numbers - Prints the numbers from 0 to 9.
+*
+*
+*/
+void print_numbers(void)
+{
+	int x = 48;
+
+	while (x < 58)
+	{
+		_putchar(x)
+	}
+	x++;
+}
