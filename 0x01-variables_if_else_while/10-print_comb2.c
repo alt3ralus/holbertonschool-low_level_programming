f845a41ca6ed6673409342988130d374c8f8c37f
alt3ralus <755@holbertonschool.com>
2019-02-18 04:51:31 +0000

    task 10 the last chapter

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,17 +8,14 @@
 int main(void)
 {
 	int x;
-	int y = 0;
 
 	for (x = 0; x < 100; x++)
 	{
-		for (y = 0; y < 10; y++)
+		putchar ((x / 10) + '0');
+		putchar ((x % 10) + '0');
+		if (x < 99)
 		{
-			putchar ((x / 10) + '0');
-			putchar ((y % 10) + '0');
-
-			if (y < 99)
-				putchar (',');
+			putchar (',');
 			putchar(' ');
 		}
 	}
