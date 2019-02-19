a51ce72797b2ef955a1e455b74d55032628849f8
alt3ralus <755@holbertonschool.com>
2019-02-19 17:45:04 +0000

    task 2 swap values

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int  - Re asign a value to n variable.
+ *
+ *@a: variable int.
+ *@b: variable int.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
