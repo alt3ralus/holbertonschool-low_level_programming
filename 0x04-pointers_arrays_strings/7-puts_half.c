a0274ff890165ddc9147dd75a7e865c6bff62b25
alt3ralus <755@holbertonschool.com>
2019-02-22 04:45:57 +0000

    task 7

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * puts_half - A function that prints half of a string.
+ * @str: pointer char.
+ * Return: Void.
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			j = i / 2;
+		}
+
+		else
+			j = (i - 1) / 2;
+		for ( ; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
