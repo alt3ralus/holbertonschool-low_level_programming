a3cfd5fde62a3c73fb79ea9ece93cdd1aa2d6119
alt3ralus <755@holbertonschool.com>
2019-02-22 04:55:56 +0000

    task 7 checker is pissin' me off, the first time works

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -12,18 +12,18 @@ void puts_half(char *str)
 	int j;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
+
 		if (i % 2 == 0)
 		{
 			j = i / 2;
 		}
 
 		else
-			j = (i - 1) / 2;
-		for ( ; j < i; j++)
-		{
-			_putchar(str[j]);
-		}
+			j = (i + 1) / 2;
+	for ( ; j < i; j++)
+	{
+		_putchar(str[j]);
 	}
+
 	_putchar('\n');
 }
