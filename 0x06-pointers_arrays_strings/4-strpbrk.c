e3beec480188c51c56c141f656ef428903a65d36
alt3ralus <755@holbertonschool.com>
2019-02-26 03:16:32 +0000

    task 4

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -8,7 +8,17 @@
  * Return: pointer.
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_strpbrk(char *s, char *accept)
 {
+	int i, j;
 
+	for (i = 0; s[i] != '\0'; i++);
+	{
+		for (j = 0; accept[j] != '\0'; j++);
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (0);
 }
