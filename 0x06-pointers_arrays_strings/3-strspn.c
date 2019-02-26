664cd45a76a3aa759bf12a79d44a28333b3c0508
alt3ralus <755@holbertonschool.com>
2019-02-26 02:00:08 +0000

    task 3

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -9,5 +9,19 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int i;
+	unsigned int j;
+	unsigned int x = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				x = x + 1;
+		}
+		if (x == i)
+			break;
+	}
+	return (x);
 }
