7664459980fc6e187b977b2a0af0d8bd0873ad14
alt3ralus <755@holbertonschool.com>
2019-02-27 14:37:36 +0000

    update

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -1,13 +1,24 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * set_string - Funt. that fills memory with a constant byte.
- * @s: double ponter
- * @to: char destination
+ * print_diagsums - Funt. that fills memory with a constant byte.
+ * @size: int size of the matrix.
+ * @a: pointer int type.
+ * @sum1: gives the sum of first diagonal.
+ * @sum2: gives the sum of the second diagonal.
  * Return: pointer.
  */
 
-void set_string(char **s, char *to)
+void print_diagsums(int *a, int size)
 {
-	*s = to;
+	int i, suma1;
+
+	for (i = 0; i < size; i += size + 1)
+	{
+		suma1 = a[i];
+	}
+	printf("%d \n", suma1);
+
+
 }
