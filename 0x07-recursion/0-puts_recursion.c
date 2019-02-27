a46f64540f2c1a01db1efabefa3cb10eb59187d2
alt3ralus <755@holbertonschool.com>
2019-02-27 17:59:39 +0000

    first try

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -1,7 +1,15 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _puts_recursion - Function that prints a string plus new line.
+ * @s: ponter to a char.
+ * Return: Void.
  */
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+		return (0);
+	else
+		_putchar(s);
+	_puts_recursion=*(s + 1);
+}
