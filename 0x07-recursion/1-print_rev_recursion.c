6ec7130ac246f38c31920e37e05603a411b93604
alt3ralus <755@holbertonschool.com>
2019-02-27 21:50:15 +0000

    just print after the recursion

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,7 +1,16 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _puts_recursion - Function that prints a string plus new line.
+ * @s: ponter to a char.
+ * Return: Void.
  */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
