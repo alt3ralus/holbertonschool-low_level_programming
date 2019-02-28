7d86811af0103b4b2f48b539ea3fb3cb9949325c
alt3ralus <755@holbertonschool.com>
2019-02-28 02:53:24 +0000

    return and betty fixed

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _strlen_recursion - A function that returns the length of the string
+ * @s: char pointer
+ * Return: int value. Len of var.
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == 0)
 	{
-		return(0);
+		return (0);
 	}
 	else
 	{
-		return(1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
