ce9b4eb266d8b142330b10296816c5984c01bba6
alt3ralus <755@holbertonschool.com>
2019-03-03 22:14:34 +0000

    task 1

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _strncat - A function that concatenates 2 str.
+ * @dest: type char.
+ * @src: type char.
+ * @n: int type.
+ * Return: pointer char.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+
+}
