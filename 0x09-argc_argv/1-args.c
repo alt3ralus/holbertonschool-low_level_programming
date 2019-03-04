84931c7039c4f016b80f92924a991f33be29ab4e
alt3ralus <755@holbertonschool.com>
2019-03-04 03:02:01 +0000

    task 2

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - A main function
+ * @argc: int type.
+ * @argv: char type.
+ * Return: int.
+ */
+int main(int argc, char  *argv[])
+{
+	argv = argv;
+
+	if (argc > 0)
+	{
+		_putchar((argc - 1) + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
