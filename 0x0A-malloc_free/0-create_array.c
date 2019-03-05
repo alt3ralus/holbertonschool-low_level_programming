1019eb89abd03362d77dab2b3a651a04620bdfb0
alt3ralus <755@holbertonschool.com>
2019-03-05 22:25:54 +0000

    task 0 done!!!

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - A Function that create an array.
+ * @size: unsigned var.
+ * @c: char var.
+ * Return: a pointer char.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *cadena;
+	unsigned int i;
+
+	cadena = malloc(sizeof(char) * size);
+
+	if (size == 0 || cadena == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		cadena[i]= c;
+	return (cadena);
+
+
+}
