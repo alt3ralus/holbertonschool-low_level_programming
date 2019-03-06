a915b08a93b972bc26fcf2d3394c0d6b08c19d33
alt3ralus <755@holbertonschool.com>
2019-03-06 00:24:19 +0000

    task 1

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup -   returns a pointer to a new allocated spaces in memory.
+ * @str: pointer char.
+ *
+ * Return: a new pointer.
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *cadena;
+
+	if (str == NULL)
+
+	for (i = 0; str[i] != '\0'; i++)
+
+
+	cadena = malloc((*str) * i);
+
+
+	for (j = 0; j < i  ; j++)
+	{
+		cadena[j] = str[j];
+	}
+	return (cadena);
+}
