887c12e15a58dedb7ef7de72d1557754f7a204c3
alt3ralus <755@holbertonschool.com>
2019-03-06 01:17:09 +0000

    task 2 preview

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * str_concat -
- *
- * Return: 
+ * str_concat - concatenates 2 str.
+ * @s1: char var.
+ * @s2: char var.
+ * Return: var char type.
  */
 char *str_concat(char *s1, char *s2)
 {
