cb0975968edf032b70886f032812604f7e705de4
alt3ralus <755@holbertonschool.com>
2019-03-07 00:23:29 +0000

    3 tested

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,11 +1,24 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * alloc_grid -
- *
- * Return: 
+ * alloc_grid - return a pointer 2D  array.
+ * @width: int
+ * @height: int
+ * Return: pointer to pointer.
  */
 int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **cadena = 0;
 
+	if (height == 0 || width == 0)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{}
+	for (j = 0; j < width; j++)
+	{}
+	return (cadena);
 }
