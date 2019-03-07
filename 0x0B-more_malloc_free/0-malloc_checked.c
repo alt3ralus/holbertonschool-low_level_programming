29a07731de09abd1835d240895c9ab085338cbbb
alt3ralus <755@holbertonschool.com>
2019-03-07 20:59:37 +0000

    task betty fix

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -12,13 +12,12 @@ void *malloc_checked(unsigned int b)
 {
 	unsigned int *puntero;
 
-
-	puntero=malloc(b);
+	puntero = malloc(b);
 
 	if (puntero == NULL)
 	{
 		exit(98);
 	}
-	return((void*)puntero);
+	return ((void *) puntero);
 
 }
