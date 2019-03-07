ba0f76fb2fb6a52b4f15ac0e336b0065b1307f8a
alt3ralus <755@holbertonschool.com>
2019-03-07 00:44:12 +0000

    added

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -1,11 +1,23 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * strtow -
- *
- * Return: 
+ * **strtow - return a pointer 2D  array.
+ * @str: char
+ * Return: pointer to pointer.
  */
 char **strtow(char *str)
 {
+	int i, j;
+	int **cadena = 0;
 
+	if (str == 0 || str == "")
+		return (NULL);
+
+	for (i = 0; i < srt; i++)
+	{}
+	for (j = 0; j < srt; j++)
+	{}
+	return (cadena);
 }
