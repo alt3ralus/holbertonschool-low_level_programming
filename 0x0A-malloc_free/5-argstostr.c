b9798bb2e85cce29aa17839908695dd90e318f72
alt3ralus <755@holbertonschool.com>
2019-03-07 00:39:16 +0000

    5 tested

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,11 +1,24 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * argstostr -
- *
- * Return: 
+ * alloc_grid - return a pointer 2D  array.
+ * @av: pointer to pointer
+ * @ac: int
+ * Return: pointer to pointer.
  */
 char *argstostr(int ac, char **av)
 {
+	int i, j;
+	char *cadena = 0;
 
+	if (ac == 0 || av  == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{}
+	for (j = 0; j < ac; j++)
+	{}
+	return (cadena);
 }
