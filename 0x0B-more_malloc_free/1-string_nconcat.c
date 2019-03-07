0ae8e2211fb6171d8d27170f8b6422e51264567e
alt3ralus <755@holbertonschool.com>
2019-03-07 22:13:47 +0000

    not finished yet

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - Allocates  memory using.
+ * @s1: char ponter to
+ * @s2: char pointer to
+ * @n: unsigned int.
+ * Return: char pointer to.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *cadena;
+	int i, j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{}
+	for  (j = 0; s2[j] != '\0'; j++)
+	{}
+	cadena =  malloc(sizeof char i + n);
+	return (s1);
+}
