53e860e702d5079a3a9a9150782b434a1705afb9
alt3ralus <755@holbertonschool.com>
2019-03-12 01:52:58 +0000

    dog header created

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure that contains several members.
+ * @name: pointer type.
+ * @age: integer type.
+ * @owner: pointer type.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
