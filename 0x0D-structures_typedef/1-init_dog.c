b9ea9a24de1137e2931ce727d4844399f5742b75
alt3ralus <755@holbertonschool.com>
2019-03-13 03:17:24 +0000

    return nul deleted and betty

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - A function that initialize a variable of type.
+ * @d: pointer to a function.
+ * @name: pointer
+ * @age:float
+ * @owner:  pointer
+ * Return: void.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+
+}
