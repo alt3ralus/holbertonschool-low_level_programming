69d9c5728fe628d930b0da5f30cc218606cad493
alt3ralus <755@holbertonschool.com>
2019-03-15 03:11:19 +0000

    correction

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -9,5 +9,4 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int main (int arc, char **argv);
 #endif
