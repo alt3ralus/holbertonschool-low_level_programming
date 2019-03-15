0108e91acb624bb022f430e10cdaebcec645cdcd
alt3ralus <755@holbertonschool.com>
2019-03-15 23:39:12 +0000

    task 1

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum any amount of numbers as argument.
+ * @n: constant var.
+ * Return: An int. The sum result.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list datos;
+	int suma = 0;
+
+	va_start(datos, n);
+
+	if (n == 0)
+	{
+		return (0);
+
+	}
+	for (i = 0; i < n; i++)
+	{
+		suma += va_arg(datos, int);
+	}
+	va_end(datos);
+	return (suma);
+}
