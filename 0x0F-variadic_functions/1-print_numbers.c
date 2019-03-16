aae7e07f17ae40349b0374d8cca8d582cb9ca030
alt3ralus <755@holbertonschool.com>
2019-03-16 19:49:59 +0000

    betty fixed

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -11,14 +11,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_list datos;
-	va_start(datos, n);
 
+	va_start(datos, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(datos, int));
 
-		if (i != n -1)
+		if (i < n - 1)
 			printf("%s", separator);
 	}
 	putchar('\n');
