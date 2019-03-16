90ccbe42cee7e6c4a532278db5b9e6617261c140
alt3ralus <755@holbertonschool.com>
2019-03-16 22:38:21 +0000

    char added

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -11,6 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list datos;
+	char * cadenaP;
 
 	va_start(datos, n);
 
@@ -18,7 +19,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		for (i = 0; i < n; i++)
 		{
-				printf("%s", va_arg(datos, char *));
+
+			cadenaP = va_arg(datos, char *);
+
+			if (cadenaP == NULL)
+			{
+				printf("%p", cadenaP);
+
+			}
+			else
+			{
+				printf("%s", cadenaP);
+			}
+
 			if (i < n - 1 && separator != NULL)
 				printf("%s", separator);
 		}
