ff9d473caf4b14e8123e1f4b92666e03593785af
alt3ralus <755@holbertonschool.com>
2019-03-17 01:15:14 +0000

    other way to

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,19 @@
-section	.text
+section .text
    global main
 
 main:
-   mov	edx,len
-   mov	ecx,msg
-   mov	ebx,1
-   mov	eax,4
-   int	0x80
+    ; Write string to stdout
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, string
+    mov edx, strlen
+    int 0x80
 
-   mov	eax,1
-   int	0x80
+    ; Exit
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
 
-section	.data
-msg db 'Hello, Holberton', 0xa
-len equ $ - msg
+section .data
+    string  db 'Hello, World!',10
+    strlen equ $ -  string
