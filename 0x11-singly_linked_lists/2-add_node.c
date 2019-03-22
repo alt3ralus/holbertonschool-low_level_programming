96b676567ee999607fec850a19fc5075e7ddaa54
alt3ralus <755@holbertonschool.com>
2019-03-22 19:10:04 +0000

    task 2 base

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -7,3 +7,28 @@
  * Return: Len of str list.
  */
 list_t *add_node(list_t **head, const char *str);
+{
+	int i;
+	list_t *nodo;
+
+	nodo = malloc(sizeof(list_t));
+	nodo->str = strdup(str);
+
+	for (i = 0; srt[i] != '\0'; i++)
+	{}
+
+	nodo->len = i;
+
+	if (nodo == NULL)
+	{
+
+	}
+
+
+
+
+
+
+
+
+}
