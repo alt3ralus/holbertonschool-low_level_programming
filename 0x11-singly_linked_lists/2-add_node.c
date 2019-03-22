b2a2b2f46d7f0b1dc2d7fd295afd8ad9f345ac3b
alt3ralus <755@holbertonschool.com>
2019-03-22 02:48:09 +0000

    task 2 base files added

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * add_node - Add one note to the list.
+ * @head: double pointer to char.
+ * @str: pointer to char.
+ * Return: Len of str list.
+ */
+list_t *add_node(list_t **head, const char *str);
