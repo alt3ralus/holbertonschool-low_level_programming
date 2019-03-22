a8dc580c49e455f23f67e633443dce6e053e768a
alt3ralus <755@holbertonschool.com>
2019-03-22 01:14:10 +0000

    betty fixed

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,23 +1,20 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_list  - check the code for Holberton School students.
+ * @h: pointer to char.
+ * Return: Len of the list.
  */
 size_t print_list(const list_t *h)
 {
 
 	int count;
 
-
-
 	for (count = 0; h != NULL; count++)
 	{
 		if (h->str != NULL)
 		{
 			printf("[%d] %s\n", h->len, h->str);
-
 		}
 		else
 		{
