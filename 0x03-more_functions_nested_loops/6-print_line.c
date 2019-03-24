962c6c2b777404eab765c0af60ac9d7933d5d438
alt3ralus <755@holbertonschhool.com>
2019-03-24 16:20:38 -0500

    task 6 done v.2

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (i < n)
+	{
+		while (i < n)
+		{
+			_putchar(95);
+			i++;
+		}
+	}
+	else
+	{}
+
+	_putchar('\n');
+
+}
