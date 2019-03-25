5f482a720756a3419e5e331bb2cc7c1f75e97e10
alt3ralus <755@holbertonschhool.com>
2019-03-25 18:39:30 -0500

    task 2

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -9,9 +9,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *nodo;
+	int count;
 
 	nodo = malloc(sizeof(list_t));
 	nodo->str = strdup(str);
+
+	for (count = 0; nodo->str[count] != '\0'; count++)
+	{
+	}
+
+	nodo->len = count;
 
 	if (*head == NULL)
 	{
@@ -22,5 +29,6 @@ list_t *add_node(list_t **head, const char *str)
 		nodo->next = *head;
 		*head = nodo;
 	}
+
 	return (nodo);
 }
