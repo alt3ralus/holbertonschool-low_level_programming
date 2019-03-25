b451a42fd4fedf7f3a6396513a98e083b5c94898
alt3ralus <755@holbertonschhool.com>
2019-03-25 16:58:27 -0500

    task 4

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - A function that adds a new node at the beginning of a
+ * @n: Data
+ * @head: pointer to pointer to a struct list.
+ * Return: the addres or new node.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *nodo = NULL;
+
+	while (head != NULL)
+	{
+		printf("%i\n",(head->n));
+		nodo = head;
+		head = nodo->next;
+		free(nodo);
+	}
+}
