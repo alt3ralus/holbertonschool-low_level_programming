9b09dc0c367b10da80494f1447089be405032472
alt3ralus <755@holbertonschhool.com>
2019-03-25 16:07:53 -0500

    task 3

---
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - A function that adds a new node at the beginning of a
+ * @n: Data
+ * @head: pointer to pointer to a struct list.
+ * Return: the addres or new node.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *nodo;
+	listint_t *temp;
+
+	nodo = malloc(sizeof(listint_t));
+	nodo->n = n;
+
+	temp = *head;
+
+	if (*head == NULL)
+	{
+		*head = nodo;
+
+	}
+	else
+	{
+		while (temp->next != NULL)
+		{
+			temp = (temp)->next;
+		}
+		temp->next = nodo;
+	}
+	return (temp);
+}
