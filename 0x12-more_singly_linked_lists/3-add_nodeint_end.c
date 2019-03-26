4ebbca76b4dca30d0bc7e77da559d028666964b8
alt3ralus <755@holbertonschool.com>
2019-03-26 17:30:37 +0000

    task 3

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,6 +12,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *temp;
 
 	nodo = malloc(sizeof(listint_t));
+
+	if (nodo == NULL)
+		return (NULL);
+
 	nodo->n = n;
 
 	temp = *head;
