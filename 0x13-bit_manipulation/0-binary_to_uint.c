a7799073368b4ab9d6b41141ad38ecb1d4d8b29a
alt3ralus <755@holbertonschool.com>
2019-03-28 21:18:15 +0000

    base task

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,16 +1,35 @@
 #include "holberton.h"
-
+#include <math.h>
 /**
  * binary_to_uint - a function that converts a binary to unsigned.
  * @b: pointer to char.
  * Return: unsigned int. Number converted.
  */
 
+
+
+
+
+unsigned int _pow(unsigned int a, unsigned int b)
+{
+   unsigned int i, r = 1;
+
+   for(i = 1; i <= b; ++i)
+       r *= a;
+
+   for(i = 0; i > b;  --i)
+       r /= a;
+
+   return r;
+}
+
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int numero = numero;
-	unsigned int count = NULL;
-
+	unsigned int count;
+	unsigned int sum = 0;
+        unsigned int var = 0;
+	unsigned int tmp = 0;
 
 	if (( *b != '0' && *b != '1') || b == NULL)
 	{
@@ -20,8 +39,16 @@ unsigned int binary_to_uint(const char *b)
 	{
 		for (count = 0; b[count] != '\0'; count++)
 		{
-			numero = 'b' - 48;
-		}
-		return (numero);
+
+			var = (int) b[count];
+			var =  var - 48;
+			printf("%i", var);
+			tmp = _pow(2,count);
+
+			printf("temp = %u\n", tmp);
+			sum += var * tmp;
+
+		} printf("\n");
+		return (sum);
 	}
 }
