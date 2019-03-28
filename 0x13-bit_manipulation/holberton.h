56df8dadfd34fe5db3ff26af2fcc61a6cb0f83e8
alt3ralus <755@holbertonschhool.com>
2019-03-28 17:53:57 -0500

    .h file

---
+++ 0x13-bit_manipulation/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+#include  <stdio.h>
+#include <stdlib.h>
+
+int _putchar(int c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+#endif
