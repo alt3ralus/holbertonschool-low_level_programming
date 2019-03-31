a4ec2d95a8f0cd87dc39f3c9952b8a3d5ca3ca70
alt3ralus <755@holbertonschool.com>
2019-03-31 19:56:03 +0000

    null condition added

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -31,6 +31,10 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	unsigned int sum = 0;
 
+	if (b == NULL)
+		return (0);
+
+
 	while (b[len] != '\0')
 	{
 		if (b[len] != '0' && b[len] != '1')
