4c8331c2639e1fd488b7559c1aeaab8ebee0fa99
alt3ralus <755@holbertonschool.com>
2019-03-31 22:25:45 +0000

    task 3

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,38 @@
+
+#include "holberton.h"
+
+/**
+ * get_bit - Print index position of a binary.
+ * @n: The int to convert.
+ * @index: position in t
+ * Return: Void.
+ */
+
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit, resultgi;
+	int cadena[64];
+
+
+	if (n == 0)
+	{
+
+		return (-1);
+	}
+
+
+	for (bit = 63; bit >= 0; bit--)
+	{
+		result = n >> bit;
+
+		if (result & 1)
+			cadena[bit] = 1;
+		else
+			cadena[bit] = 0;
+
+	}
+
+	return (cadena[index]);
+
+}
