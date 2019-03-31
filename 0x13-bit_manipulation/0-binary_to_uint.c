c154bd459af2b46170c07d89e47d782f3e48eefb
alt3ralus <755@holbertonschool.com>
2019-03-31 19:45:29 +0000

    task 0 fixed

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * binary_to_uint - a function that converts a binary to unsigned.
- * @b: pointer to char.
- * Return: unsigned int. Number converted.
+ * _pow  - a function that converts a binary to unsigned.
+ * @a: int.
+ * @b: int.
+ * Return: pow of a number.
  */
 unsigned int _pow(unsigned int a, unsigned int b)
 {
@@ -14,31 +15,41 @@ unsigned int _pow(unsigned int a, unsigned int b)
 	for (i = 0; i > b;  --i)
 		r /= a;
 
-	return (0);
+	return (r);
 }
+
+/**
+ * binary_to_uint - a function that converts a binary to unsigned.
+ * @b: pointer to char.
+ * Return: unsigned int. Number converted.
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
 	int len = 0;
-
-	while (b[len] != '\0')
-	{
-		if (b[len] != '0' &&  b[len] != '1')
-		{
-			return (0);
-		}
-		len++;
-	}
 
 	unsigned int exp = 0;
 	unsigned int num = 0;
 	unsigned int sum = 0;
 
+	while (b[len] != '\0')
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		len++;
+	}
+
+
 	for (len = len - 1; len >= 0; len--)
 	{
 
 		num = (int)b[len] - 48;
+
 		sum = sum + num * _pow(2, exp);
+
 		exp++;
 	}
+
 	return (sum);
 }
