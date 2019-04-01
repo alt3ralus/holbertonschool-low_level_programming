7b7d4e98b8c1a9784f58bcb1ab9187857ad2412e
alt3ralus <755@holbertonschool.com>
2019-04-01 01:25:55 +0000

    task 3

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_node_end - A function that adds a new node at the beginning of a
+ * @head: pointer to pointer to a struct list.
+ * @str: string.
+ * Return: the addres or new node.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *nodo;
+	list_t *temp;
+
+	nodo = malloc(sizeof(list_t));
+	temp = malloc(sizeof(list_t));
+
+	if (nodo == NULL)
+		return (NULL);
+
+	nodo->str = strdup(str);
+
+	if (*head == NULL)
+	{
+		*head = nodo;
+		nodo->next = NULL;
+	}
+
+	temp = *head;
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = nodo;
+	nodo->next = NULL;
+
+	return (temp);
+}
