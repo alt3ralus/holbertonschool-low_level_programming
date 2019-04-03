2aa1af80126c68e75e25a8f7800f4a0d2970c9b0
alt3ralus <755@holbertonschool.com>
2019-04-03 04:45:44 +0000

    task 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,8 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = creat(filename, 0600);
-
+	file = creat(filename, O_EXCL | 0600);
 
 	if (text_content == NULL)
 	{
