60173b113a935a5b75ac3006a83a08d12dd69871
alt3ralus <755@holbertonschool.com>
2019-04-03 04:41:54 +0000

    task 1 fixed creat

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -7,12 +7,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, esc, count = 0;
+	int file;
+	int esc, count = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_TRUNC | O_WRONLY, 0600);
+	file = creat(filename, 0600);
 
 
 	if (text_content == NULL)
