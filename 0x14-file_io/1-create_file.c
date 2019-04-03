5d074c9cdd392f61230b00464fda64616cd637f1
alt3ralus <755@holbertonschool.com>
2019-04-03 14:56:40 +0000

    [change] macro modified

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,11 +14,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 
-	file = creat(filename, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	while (text_content[count] != '\0')
 	{
-
 		count++;
 	}
 
