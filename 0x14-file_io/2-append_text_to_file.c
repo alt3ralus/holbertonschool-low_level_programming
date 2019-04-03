8f8a730bf6c024b09ccb4d03ca8d7f29bd540b6b
alt3ralus <755@holbertonschool.com>
2019-04-03 16:03:54 +0000

    task 2 test

---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - add text to a file.
+ * @filename: pointer to the file.
+ * @text_content: The size of the letter to print.
+ * Return: thr amount of chars to print.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	int esc, count = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND | O_TRUNC, 0600);
+
+	if (text_content == NULL)
+	{
+		close(file);
+		return (1);
+	}
+
+	while (text_content[count] != '\0')
+	{
+		count++;
+	}
+
+	esc = write(file, text_content, count);
+
+	if (file == -1 || esc == -1)
+	{
+		return (-1);
+	}
+
+	close(file);
+
+	return (1);
+
+}
