c8ea8b9c8b84cd98e4229be806e4801347986c89
alt3ralus <755@holbertonschool.com>
2019-04-03 05:28:36 +0000

    task 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,12 +13,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-
-	{
-		file = creat(filename, 0600);
-		return (-1);
-	}
 
 	file = creat(filename, 0600);
 
@@ -27,6 +21,9 @@ int create_file(const char *filename, char *text_content)
 
 		count++;
 	}
+
+	if (text_content == NULL)
+		return (1);
 
 	esc = write(file, text_content, count);
 
