67c6705cd278fc733871e90a48654a2763e6f9d8
alt3ralus <755@holbertonschool.com>
2019-04-04 16:16:37 +0000

    conditions changed

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -15,8 +15,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_APPEND | O_EXCL, 0600);
-
 	if (text_content == NULL)
 	{
 		close(file);
@@ -26,7 +24,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		close(file);
 		return (1);
-	}
+
+	file = open(filename, O_WRONLY | O_APPEND);
 
 	while (text_content[count] != '\0')
 	{
