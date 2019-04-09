c0ad16382fca17480dbec44e5c0c4ccd6a271ffa
alt3ralus <755@holbertonschool.com>
2019-04-09 04:42:48 +0000

    indent fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,33 +7,33 @@
 
 int main(void)
 {
-        int x = 48, y = 48, z;
+	int x = 48, y = 48, z;
 
-
-        while (x <= 57)
-        {
-                for  ( ; y <= 57; y++)
-                {
-                        for (; z <= 57; z++)
-                        {
-                                if (x != y && y != z && z != x)
-                                {
-                                        putchar(x);
-                                        putchar(y);
-                                        putchar(z);
-                                        {
-                                            if (x != 55 || y != 56 || z != 57)
-                                                putchar(',');
-                                                putchar(' ');
-                                        }
-                                }
-                        }
-                        z = y + 1;
-                }
-                y = x + 1;
-                x++;
-        }
-        putchar('\n');
-
-        return (0);
+	while (x <= 57)
+	{
+		for  ( ; y <= 57; y++)
+		{
+			for (; z <= 57; z++)
+			{
+				if (x != y && y != z && z != x)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					{
+						if (x != 55 || y != 56 || z != 57)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+			z = y + 1;
+		}
+		y = x + 1;
+		x++;
+	}
+	putchar('\n');
+	return (0);
 }
