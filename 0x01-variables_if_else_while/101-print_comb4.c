8afe3e9b0491a43ef8fafa38dca30e03342b0b67
alt3ralus <755@holbertonschool.com>
2019-04-09 04:33:53 +0000

    comma fixed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,39 +5,35 @@
  * Return: zero if success.
  */
 
-#include <stdio.h>
-
 int main(void)
 {
-	int x = 48, y = 48, z;
+        int x = 48, y = 48, z;
 
 
-	while (x <= 57)
-	{
-		for  ( ; y <= 57; y++)
-		{
-			for (; z <= 57; z++)
-			{
+        while (x <= 57)
+        {
+                for  ( ; y <= 57; y++)
+                {
+                        for (; z <= 57; z++)
+                        {
+                                if (x != y && y != z && z != x)
+                                {
+                                        putchar(x);
+                                        putchar(y);
+                                        putchar(z);
+                                        {
+                                            if (x != 55 || y != 56 || z != 57)
+                                                putchar(',');
+                                                putchar(' ');
+                                        }
+                                }
+                        }
+                        z = y + 1;
+                }
+                y = x + 1;
+                x++;
+        }
+        putchar('\n');
 
-				if (x != y && y < 58)
-				{
-					putchar(x);
-					putchar(y);
-					putchar(z);
-
-					if (x != 55 || y != 56 || z != 57)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-			z = y + 1;
-		}
-		y = x + 1;
-		x++;
-	}
-	putchar('\n');
-
-	return (0);
+        return (0);
 }
