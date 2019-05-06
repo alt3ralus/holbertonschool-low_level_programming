ba05795e82eb2ba55960574ffc731d147623ba86
alt3ralus <755@holbertonschool.com>
2019-05-06 04:40:04 +0000

    task 2 preview

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds not to start of dlinked list
+ * @head: double ointer to list
+ * @n: an int.
+ * Return: the address of the new element, or NULL if it failed.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
