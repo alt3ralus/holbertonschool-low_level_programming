2759f584098b33b14bc60ed12c392a98f1f4fdc9
alt3ralus <755@holbertonschool.com>
2019-06-01 02:59:37 +0000

    betty fixed

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,9 +1,10 @@
 #include "hash_tables.h"
 /**
- *
- *
- *
+ * hash_table_create - A fun ha creates a hash array.
+ * @size:size of the array.
+ * Return: Pointer to array.
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *index = NULL;
@@ -18,12 +19,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 
 	index->size = size;
-	index->array = malloc(sizeof(hash_node_t *)* size);
+	index->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (index->array == NULL)
 		return (NULL);
 
-	return(index);
-
-
+	return (index);
 }
