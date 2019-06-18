528f7d904f1b1dec8b68ee96b19f37a239ba4615
alt3ralus <755@holbertonschool.com>
2019-06-18 00:55:02 +0000

    base files

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * bubble_sort - that sorts an array of integers in ascending order
+ * @array: array of int.
+ * @size: the ssize of array
+ * Return: Void
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
