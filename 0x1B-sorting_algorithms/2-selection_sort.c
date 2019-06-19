1cbe87e6713bf007c524e77e83c78f5debd102a8
alt3ralus <755@holbertonschool.com>
2019-06-19 22:01:44 +0000

    betty fixed indent

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -15,20 +15,23 @@ void selection_sort(int *array, size_t size)
 		return;
 	}
 
-	for (count_a= 0; count_a < size - 1; count_a++)
+	for (count_a = 0; count_a < size - 1; count_a++)
 	{
 		value = count_a;
+
 		for (count_b = count_a + 1; count_b < size; count_b++)
 		{
 			if (array[value] > array[count_b])
+			{
 				value = count_b;
+			}
 		}
 		if (value != count_a)
-			{
-				aux = array[count_a];
-				array[count_a] = array[value];
-				array[value] = aux;
-				print_array(array, size);
-			}
+		{
+			aux = array[count_a];
+			array[count_a] = array[value];
+			array[value] = aux;
+			print_array(array, size);
+		}
 	}
 }
