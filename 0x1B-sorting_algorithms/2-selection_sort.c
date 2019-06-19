10635bd9f3fc1af2932fb75b2033e4574fe123df
alt3ralus <755@holbertonschool.com>
2019-06-19 21:52:01 +0000

    betty fixed

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -7,25 +7,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t countA, countB, aux;
-	size_t value;
+	size_t count_a = 0, count_b = 0, aux = 0;
+	size_t value = 0;
 
-	if (!array)
-		return;
-	for (countA = 0; countA + 1 < size; countA++)
+	if (array == NULL)
 	{
-		value = countA;
-		for (countA = countB + 1; countB < size; countB++)
+		return;
+	}
+
+	for (count_a= 0; count_a < size - 1; count_a++)
+	{
+		value = count_a;
+		for (count_b = count_a + 1; count_b < size; count_b++)
 		{
-			if (array[value] > array[countB])
-				value = countA;
+			if (array[value] > array[count_b])
+				value = count_b;
 		}
-		if (value != countA)
-		{
-			aux = array[countA];
-			array[countA] = array[value];
-			array[value] = aux;
-			print_array(array, size);
-		}
+		if (value != count_a)
+			{
+				aux = array[count_a];
+				array[count_a] = array[value];
+				array[value] = aux;
+				print_array(array, size);
+			}
 	}
 }
