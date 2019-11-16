046c17ed45f61a2325d04bbf0edea8bf187ed9da
alt3ralus <47127251+alt3ralus@users.noreply.github.com>
2019-11-15 23:17:18 -0500

    just tested

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ * bubble_sort - that sorts an array of integers in ascending order
+ * @array: array of int.
+ * @size: the ssize of array
+ * Return: Void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t a = 0, b = 0, tmp = 0;
+
+
+	if (array == NULL)
+		return;
+	if (size < 2)
+		return;
+
+	for (a = 0; a < size - 1; a++)
+	{
+		for (b = 0; b < size - a - 1; b++)
+		{
+			if (array[b] > array[b + 1])
+			{
+				tmp = array[b];
+				array[b] = array[b + 1];
+				array[b + 1] = tmp;
+				print_array(array, size);
+			}
+
+
+		}
+
+
+	}
+
+}
